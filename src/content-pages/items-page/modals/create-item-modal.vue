96c8a3e9a1ba7eb75ef687f28647d7677c768961
Reverie <patlavovach@gmail.com>
2022-06-30 02:39:33 +0300

    moved item form to separate SFC

--- src/content-pages/items-page/modals/create-item-modal.vue
+++ src/content-pages/items-page/modals/create-item-modal.vue
@@ -1,39 +1,19 @@
 <script setup>
   import uniqid from 'uniqid'
   import { useItemsStore } from '../../../stores/items'
-  import { watch } from 'vue'
-  import useValidate from '@vuelidate/core'
-  import { required, minValue } from '@vuelidate/validators'
+  import ItemForm from './forms/item-form'
+
   let opened = $ref(false)
   const open = () => (opened = true)
 
+  const form = $ref(null)
+
   const { addItem } = useItemsStore()
 
-  const item = $ref({
-    name: '',
-    description: '',
-    weight: 0,
-    price: 0
-  })
-  const rules = {
-    name: { required },
-    description: { required },
-    price: { required, minValue: minValue(0.01) },
-    weight: { required, minValue: minValue(0.01) }
-  }
-
-  const v$ = $ref(useValidate(rules, item))
-
-  watch(
-    () => opened,
-    value => {
-      if (!value) v$.$reset()
-    }
-  )
-
   const createItem = async () => {
-    const isValid = await v$.$validate()
+    const isValid = await form.validate()
     if (!isValid) return
+    const item = form.getItem()
     const _payload = { ...item, ...{ id: uniqid() } }
     addItem(_payload)
     opened = false
@@ -57,52 +37,8 @@
         />
       </template>
 
-      <v-card-text>
-        <v-text-field
-          v-model="item.name"
-          label="Name"
-          variant="outlined"
-          density="compact"
-          :error="v$.name.$error"
-          :error-messages="v$.name.$errors[0]?.$message"
-        />
-        <v-textarea
-          v-model="item.description"
-          variant="outlined"
-          rows="3"
-          density="compact"
-          no-resize
-          label="Description"
-          :error="v$.description.$error"
-          :error-messages="v$.description.$errors[0]?.$message"
-        />
-        <v-row>
-          <v-col cols="6">
-            <v-text-field
-              v-model.number="item.price"
-              suffix="UAH"
-              label="Price"
-              variant="outlined"
-              density="compact"
-              type="number"
-              :error="v$.price.$error"
-              :error-messages="v$.price.$errors[0]?.$message"
-            />
-          </v-col>
-          <v-col cols="6">
-            <v-text-field
-              v-model.number="item.weight"
-              label="Weight"
-              suffix="g"
-              variant="outlined"
-              density="compact"
-              type="number"
-              :error="v$.weight.$error"
-              :error-messages="v$.weight.$errors[0]?.$message"
-            />
-          </v-col>
-        </v-row>
-      </v-card-text>
+      <item-form ref="form" />
+
       <v-card-actions class="pa-6">
         <v-spacer />
         <v-btn
@@ -126,13 +62,5 @@
 <style>
   .mdi-window-close {
     cursor: pointer;
-  }
-
-  div.v-text-field .v-input__details {
-    padding-inline-start: 0;
-  }
-
-  span.v-text-field__suffix {
-    opacity: 1;
   }
 </style>
---
+++ src/content-pages/items-page/modals/forms/item-form.vue
@@ -0,0 +1,88 @@
+<script setup>
+  import { minValue, required } from '@vuelidate/validators'
+  import useValidate from '@vuelidate/core'
+
+  let item = $ref({
+    name: '',
+    description: '',
+    weight: 0,
+    price: 0
+  })
+
+  const rules = {
+    name: { required },
+    description: { required },
+    price: { required, minValue: minValue(0.01) },
+    weight: { required, minValue: minValue(0.01) }
+  }
+
+  const v$ = $ref(useValidate(rules, item))
+
+  const validate = () => v$.$validate()
+
+  const getItem = () => item
+
+  defineExpose({
+    validate,
+    getItem
+  })
+</script>
+
+<template>
+  <v-card-text>
+    <v-text-field
+      v-model="item.name"
+      label="Name"
+      variant="outlined"
+      density="compact"
+      :error="v$.name.$error"
+      :error-messages="v$.name.$errors[0]?.$message"
+    />
+    <v-textarea
+      v-model="item.description"
+      variant="outlined"
+      rows="3"
+      density="compact"
+      no-resize
+      label="Description"
+      :error="v$.description.$error"
+      :error-messages="v$.description.$errors[0]?.$message"
+    />
+    <v-row>
+      <v-col cols="6">
+        <v-text-field
+          v-model.number="item.price"
+          suffix="UAH"
+          label="Price"
+          variant="outlined"
+          density="compact"
+          type="number"
+          :error="v$.price.$error"
+          :error-messages="v$.price.$errors[0]?.$message"
+        />
+      </v-col>
+      <v-col cols="6">
+        <v-text-field
+          v-model.number="item.weight"
+          label="Weight"
+          suffix="g"
+          variant="outlined"
+          density="compact"
+          type="number"
+          :error="v$.weight.$error"
+          :error-messages="v$.weight.$errors[0]?.$message"
+        />
+      </v-col>
+    </v-row>
+  </v-card-text>
+</template>
+
+<style>
+  div.v-text-field .v-input__details {
+    padding-inline-start: 0;
+  }
+
+  span.v-text-field__suffix {
+    opacity: 1;
+  }
+</style>
